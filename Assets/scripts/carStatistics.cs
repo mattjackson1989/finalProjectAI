c1e0a2858d55dcaafcd6f079f45f5bd4b71b8726
Matt Jackson <thrashemcatchem@yahoo.com>
2017-04-19 17:24:14 -0400

    All of the assets for boxcar3d
    3 scripts: wheel script, gameEngine, carStats
    How to load:
    
    Load scene into unity and it should do the rest

---
+++ Assets/scripts/carStatistics.cs
@@ -0,0 +1,79 @@
+﻿/*  This is the main script for the car object
+ *  this should handle everything about the car: the velocity, torque, size, parts, score
+ * 
+ *  TODO: Seperate functionalities from start into their own functions
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carStatistics : MonoBehaviour {
+    // Fitness score of car
+    public float fitnessScore;
+    
+    // Actually stats for combustion
+    public float velocity;
+    public float torque;
+    // Wheel Section
+    public float wheelSize; // size of the instantiated wheel
+    //public GameObject[] wheels; // list of game objects as wheels
+    public GameObject wheelObj;
+    public GameObject instWheel; // instantiated tire
+    // Frame Section
+    public GameObject[] frames; // list of game objects as frames
+    public GameObject frame; // the actual fram chosen
+    public GameObject instFrame; // instantiated frame
+
+	// Use this for initialization
+	void Start () {
+        // this generates a random vehicle with no genetic algorithm included
+        generateRandomCar();  
+    }
+	private void generateRandomCar()
+    {
+        
+        // frame generation
+        int random = Random.Range(0, 100) % 2; // random selection for frame
+        frame = frames[random]; // pick a random frame
+        instFrame = Instantiate(frame, this.transform.localPosition, Quaternion.identity); // representation of the current object
+        
+        instFrame.transform.SetParent(this.transform); // set parent to this instantiation of carStatistics
+        instFrame.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f);
+        // wheel generation
+        // access the wheel placeholder - randomly generates which wheel placeholders will be active
+        foreach (Transform child in instFrame.transform)
+            foreach (Transform grandchild in child) // check the wheel place holders
+                if (Random.Range(0, 100) % 2 == 0) // wheel is off
+                    grandchild.gameObject.SetActive(false);
+        // now place a wheel if the placeholder exists
+        foreach (Transform child in instFrame.transform)
+        {
+            foreach (Transform grandchild in child)
+            { // check the wheel place holders
+                if (grandchild.gameObject.activeSelf)
+                { // is wheel-placeholder ON/ENABLED
+                    Debug.Log("Placed wheel");
+                    instWheel = Instantiate(wheelObj, grandchild.gameObject.transform.position, Quaternion.identity); // place at placeholders
+                    //Rotate the wheel 90 degrees
+                    instWheel.transform.Rotate(0, 0, 90);
+                    // Now set the hinge joint to the FRAME
+                    instWheel.GetComponent<HingeJoint>().connectedBody = instFrame.transform.GetChild(0).GetComponent<Rigidbody>();
+                    // set the parent of the wheel to the Car class - they must be siblings with frame
+                    instWheel.transform.parent = this.transform;
+                    // random wheel size
+                    instWheel.transform.localScale += new Vector3(Random.Range(0.01f, 0.5f), 0, Random.Range(0.01f, 0.5f));
+                    
+                }
+            }
+        }
+        // Now place it in the population array object
+       // transform.parent = this.transform;
+        // after generation, isActive must be false until it is called again to run the simulation
+        //this.gameObject.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
---
+++ Assets/scripts/gameEngine.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameEngine : MonoBehaviour {
+   
+   
+
+    public static int populationSize;
+    public GameObject[] population;
+    
+    public static float[] fitnessScores;
+    // if the car has hit the ground
+    public static bool hasStarted;
+    // if the generation is done
+    public bool genOver;
+    public GameObject car; // car game object
+    public GameObject start;
+    
+    public int currentCar; // index of the population array that is currently running simulation
+    static public bool simRunning;
+	// Use this for initialization
+	void Start () {
+        
+        populationSize = 3;
+        currentCar = 0;
+        generatePopulation(); // generate the population
+        hasStarted = false;
+        runSimulation();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(hasStarted == true && genOver != true)
+        {
+            if(population[currentCar].transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.1)
+            {
+                Debug.Log("Stopped sim");
+                // TODO: before setting inactive, you must record the fitness score of the car
+                population[currentCar].gameObject.SetActive(false);
+                currentCar++; // increase to next car in the array
+                if (currentCar != populationSize)
+                {
+                    generatePopulation();
+                    hasStarted = false;
+                    runSimulation();
+                }else
+                {
+                    hasStarted = false;
+                    genOver = true;
+                    Debug.Log("Generation Over");
+                }
+            }          
+        }else
+        {
+            if(genOver != true)
+                runSimulation(); // let a single car run a simulation
+        }
+        
+    }
+    // ---------------------------------------------------------------- Genetic Algorithm functions---------------------------------------------------------------------- //
+    public void generatePopulation()
+    {
+        population = new GameObject[populationSize];
+        fitnessScores = new float[populationSize];
+        // This loop creates the car population, runs the simulation while calculating fitness score
+        for (int i = 0; i < populationSize; i++)
+        {
+            // generate a car
+            population[i] = generateCar();
+            // give it a unique name
+            population[i].name += "_" + i;
+            population[i].gameObject.SetActive(false);
+            
+        }
+        
+    }
+
+    private void runSimulation()
+    {
+        // Car will attempt to go through the track
+        population[currentCar].gameObject.SetActive(true);
+        // calculate the fitness score
+        
+        //fitnessScores[index] = gatherFitnessScore();
+    }
+
+    public float gatherFitnessScore()
+    {
+        // TODO: Calculate the score based on the cars distance
+        // idea: randomly generate track to continously allow for distance testing
+        return 0;
+    }
+
+    public void selectCars()
+    {
+        // determine highest rated cars into new array
+    }
+    private void crossOver()
+    {
+        // combined the traits of the two cars
+    }
+    private void evaluateTarget()
+    {
+        // check if the target reached a certain distance
+    }
+    private GameObject generateCar()
+    {
+       
+        return Instantiate(car, start.transform.localPosition, Quaternion.identity);
+    }
+}
